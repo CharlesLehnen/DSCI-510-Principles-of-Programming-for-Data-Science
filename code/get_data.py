90224d9efa202d1cec9366e9e3cc77460574954d
CharlesLehnen <lehnen@usc.edu>
2022-12-09 13:21:28 -0800

    Trying to figure out why code is running indefinitely
    - Video frame and image saving debugs were in the wrong line....
    
    - While loop was stuck running, so changed it so it would stop
    
    -NOPE it was the youtube-dl code that was running indefinitely! Fixed it

--- code/get_data.py
+++ code/get_data.py
@@ -4,11 +4,12 @@ import subprocess
 import cv2
 import ffmpeg
 import youtube_dl
+import io
 
 # SET PARAMETERS
 
-## Interval between screencaptures (seconds)
-interval = 15
+INTERVAL = 15
+MAX_RUNTIME = 20
 
 
 
@@ -29,7 +30,7 @@ image_folder = os.path.join("data", "images" + "_" + second)
 if not os.path.exists(image_folder):
     os.mkdir(image_folder)
     
-## Double-check folders
+## Debug
 if os.path.exists(video_folder):
     print("Video folder created: {}".format(video_folder))
 else:
@@ -39,63 +40,81 @@ if os.path.exists(image_folder):
     print("Image folder created: {}".format(image_folder))
 else:
     print("Error creating image folder: {}".format(image_folder))
+
+
     
-if len(os.listdir(video_folder)) > 0:
-    print("Video frames saved to: {}".format(video_folder))
-else:
-    print("Error saving video frames to: {}".format(video_folder))
-
-if len(os.listdir(image_folder)) > 0:
-    print("Images saved to: {}".format(image_folder))
-else:
-    print("Error saving images to: {}".format(image_folder))
-
-'''
-
 # Download video
 
-stream = youtube_dl.YoutubeDL({"format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"}).extract_info(url, download=False)
+try:
+    stream = youtube_dl.YoutubeDL({"format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best", "timeout": 10}).extract_info(url)
+except youtube_dl.utils.DownloadError:
+    print("Failed to download video information in the allotted time.")
+
+print("done with stream step")
 
 #might need to do this, but will download to my local machine which is clunkier
 #output = youtube_dl.YoutubeDL({"format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"}).extract_info(url, download=True)
 
-## Check to see if the download worked or not
+## Debug
 if 'returncode' in stream and stream['returncode'] != 0:
     print("youtube-dl command failed with exit code {}".format(stream['returncode']))
     
 
 
-# Set up video capture using ffmpeg and cv2
+# Set up video capture using ffmpeg
 ffmpeg_command = "ffmpeg -i {} -acodec copy -vcodec copy -f mpegts pipe:".format(stream["url"])
 ffmpeg_process = subprocess.Popen(ffmpeg_command.split(), stdout=subprocess.PIPE)
-cap = cv2.VideoCapture(ffmpeg_process.stdout)
+print("done with ffmpeg step")
 
-# Debug
-print("Video capture object:")
-print(cap)
-ret, frame = cap.read()
-print("Frame data:")
-print(frame)
+# Trying to use this buffer process to fix error
+buffer = io.BytesIO()
+buffer.write(ffmpeg_process.stdout.read())
+buffer.seek(0)
+cap = cv2.VideoCapture(buffer)
+print("done with buffer step")
 
-# Capture video clips and extract images
-counter = 0
+# Get correct video stream
+video_stream = cap.get(cv2.CAP_PROP_POS_AVI_RATIO)
+print("done with video_stream step")
+
+'''
+
+# Capture frames from the stream
+
+start_time = time.time()
+print(start_time)
+
 while cap.isOpened():
     # Capture a frame
-    ret, frame = cap.read()
+    ret, frame = cap.read(video_stream)
     if ret:
         # Save the frame
         cv2.imwrite(os.path.join(video_folder, "clip_{:05d}.png".format(counter)), frame)
+        
+        ## Debug
+        if len(os.listdir(video_folder)) > 0:
+            print("Video frames saved to: {}".format(video_folder))
+        else:
+            print("Error saving video frames to: {}".format(video_folder))
 
         # Extract an image
         cv2.imwrite(os.path.join(image_folder, "image_{:05d}.png".format(counter)), image)
+        
+        ## Debug
+        if len(os.listdir(image_folder)) > 0:
+            print("Images saved to: {}".format(image_folder))
+        else:
+            print("Error saving images to: {}".format(image_folder))
 
         # Sleep
-        time.sleep(interval)
+        time.sleep(INTERVAL)
         counter += 1
-    else:
-        break
-
-# Stop video capture
-cap.release()
-
+       
+    # Stop capture
+    elapsed_time = time.time() - start_time
+    if elapsed_time <= MAX_RUNTIME:
+        cap.release()
+        
+        
 '''
+     
