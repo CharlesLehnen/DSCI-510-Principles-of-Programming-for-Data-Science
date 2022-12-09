116d3e13850861c62663a4af174db44a12d03bfb
CharlesLehnen <lehnen@usc.edu>
2022-12-08 21:31:47 -0800

    making sure folder exists

--- code/get_data.py
+++ code/get_data.py
@@ -9,3 +9,14 @@ interval = 30
 
 # Set the name of the folder for images
 folder_name = "images" + "_" + url
+
+# Create new folder if does not exist
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+while True:
+    # Use ffmpeg to extract an image from the video at the specified time interval
+    os.system(f"ffmpeg -i {url} -vf fps=1/{interval} -vframes 1 {folder_name}/image.jpg")
+
+    # Sleep for the specified interval before extracting another image
+    time.sleep(interval)
