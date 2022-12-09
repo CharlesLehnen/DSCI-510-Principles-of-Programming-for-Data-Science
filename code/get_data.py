7a51f5b3db876adc45ae03e0cf96fb26be9254e1
CharlesLehnen <lehnen@usc.edu>
2022-12-09 00:06:00 -0800

    I realized the issue was that youtube-dl was not installed in my conda environment. got farther but have the error: At least one output file must be specified
    pip install youtube-dl

--- code/get_data.py
+++ code/get_data.py
@@ -7,7 +7,7 @@ url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
 first, second = url.split('=')
 
 # Use youtube-dl to extract the streaming URL of the video
-stream_url = os.system(f"youtube-dl -g -f worst {url} > stream-url")
+os.system(f"youtube-dl -g -f worst {url} > stream-url")
 
 # Read the contents of the stream-url file using subprocess
 with open("stream-url", "r") as f:
@@ -24,11 +24,11 @@ if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
 while True:
-    # Generate timestamp for image
+    # Generate timestamp for image naming
     timestamp = time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())
 
     # Use ffmpeg to try to extract images at set intervals
-    os.system(f"ffmpeg -i {stream_url_contents} -copyts {folder_name}/img22_{timestamp}.jpeg")
+    os.system(f"ffmpeg -i {stream_url_contents} -vcodec copy {folder_name}/img22_{timestamp}.jpeg")
 
     # Sleep between extractions
     time.sleep(interval)
