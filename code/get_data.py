b63817c2bf99934b48b02005c592945fc2293372
CharlesLehnen <lehnen@usc.edu>
2022-12-08 21:56:40 -0800

    figured out windows did not like folder name with special characters in it

--- code/get_data.py
+++ code/get_data.py
@@ -3,20 +3,21 @@ import time
 
 # Set URL of YouTube live video
 url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
+first, second = url.split('=')
 
 # Set the time interval (in seconds) for image extraction
-interval = 30
+interval = 10
 
 # Set the name of the folder for images
-folder_name = "images" + "_" + url
+folder_name = "images" + "_" + second
 
 # Create new folder if does not exist
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
 while True:
-    # Use ffmpeg to extract an image from the video at the specified time interval
+    # Use ffmpeg to try to extract images at set intervals
     os.system(f"ffmpeg -i {url} -vf fps=1/{interval} -vframes 1 {folder_name}/image.jpg")
 
-    # Sleep for the specified interval before extracting another image
+    # Sleep between extractions
     time.sleep(interval)
