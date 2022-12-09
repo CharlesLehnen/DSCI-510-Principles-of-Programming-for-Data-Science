83526cb214f8923c23070152570cb538c4c2dc19
CharlesLehnen <lehnen@usc.edu>
2022-12-09 01:28:12 -0800

    working on some trouble shooting. I think the issue is that there aren't quotes around the URL for the ffmpeg command

--- code/get_data.py
+++ code/get_data.py
@@ -8,14 +8,14 @@ url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
 first, second = url.split('=')
 
 # Use youtube-dl to extract the streaming URL of the video
-output = subprocess.run(["youtube-dl", "-g", "-f", "worst", url], capture_output=True)
+url_output = subprocess.run(["youtube-dl", "-g", "-f", "worst", url], capture_output=True)
 
 # Save the streaming URL to a file
 with open("stream-url", "w") as f:
-    f.write(output.stdout.decode("utf-8"))
+    f.write(url_output.stdout.decode("utf-8"))
 
 # Set the time interval (in seconds) for image extraction
-interval = 10
+interval = 5
 
 # Set the name of the folder for images
 folder_name = "images" + "_" + second
@@ -29,8 +29,26 @@ while True:
     # Generate timestamp for image naming
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
 
-    # Use ffmpeg to try to extract images at the set intervals
-    os.system(f"ffmpeg -i {output.stdout.decode('utf-8')} -vcodec copy {folder_name}/img_{timestamp}.jpeg")
-
+    # Use ffmpeg to try to extract images at set intervals
+    #os.system(f"ffmpeg -i {url_output.stdout.decode('utf-8')} -vcodec copy {folder_name}/img_{timestamp}.jpeg")
+    
     # Sleep between extractions
     time.sleep(interval)
+    
+    
+    
+    #os.system('ffmpeg -i {url_output.stdout.decode('utf-8')} -vcodec copy {folder_name}/img_{timestamp}.jpeg'.format(folder_name=folder_name, timestamp=timestamp))
+    
+
+    
+    print(f"{url_output.stdout.decode('utf-8')}")
+    
+    print('"{url_output.stdout.decode('utf-8')}"'.format(url_output=url_output))
+    
+    print('"{folder_name}/img_{timestamp}.jpeg"'.format(folder_name=folder_name, timestamp=timestamp))
+    
+    
+    
+    
+#this is just what I was using to troubleshoot in the CL
+#ffmpeg -i "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1670598186/ei/yvmSY-XjDtaBsfIPq_-RiAI/ip/104.175.212.63/id/ydYDqZQpim8.37/itag/91/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D139/sgovp/gir%3Dyes%3Bitag%3D160/hls_chunk_host/rr2---sn-a5mekn6z.googlevideo.com/playlist_duration/30/manifest_duration/30/vprv/1/playlist_type/DVR/initcwndbps/2380000/mh/XM/mm/44/mn/sn-a5mekn6z/ms/lva/mv/m/mvi/2/pl/18/dover/11/pacing/0/keepalive/yes/fexp/24001373,24007246/mt/1670576192/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,vprv,playlist_type/sig/AOq0QJ8wRgIhANQUpHE4V_zhA_as4o5KguaZziQLhrkivnGYEqrjuNStAiEAmQZz2vxsw-Mf4yRZylO-xqFP6aFjtQyzQK_mwWJjyrQ%3D/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRAIgL-dohp-FTx7R9RauV4yV2j1RPaTKq3Q8H3JOkfIDJh4CIERa72KWWRd48CPtMIOK6aovSNJJqpTWijC6vCUI52xn/playlist/index.m3u8" -vcodec copy images_ydYDqZQpim8/img_20221209_010307_821231.jpeg
