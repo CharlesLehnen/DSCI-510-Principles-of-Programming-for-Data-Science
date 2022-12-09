d6ae2e882e56df41fc514aa07a8e67fb19c7e833
CharlesLehnen <lehnen@usc.edu>
2022-12-09 03:48:17 -0800

    Was able to get the metadata using youtube-dl, but can't get any images to get output as I iterate over the metadata...
    also added some print statements to track what is going on

--- code/get_data.py
+++ code/get_data.py
@@ -1,4 +1,6 @@
+import json
 import os
+import requests
 import time
 import subprocess
 
@@ -6,52 +8,53 @@ import subprocess
 url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
 first, second = url.split('=')
 
-# Use youtube-dl to extract the streaming URL of the video
-url_output = subprocess.run(["youtube-dl", "-g", "-f", "worst", url], capture_output=True)
-
 # Set the time interval (in seconds) for image extraction
-interval = 100
+interval = 10
 
 # Set the name of the folder for images
-folder_name = "images" + "_" + second
+screencaptures_folder = "images" + "_" + second
+if not os.path.exists(screencaptures_folder):
+    subprocess.run(["C:\\Windows\\System32\\cmd.exe", "/c", "mkdir", screencaptures_folder])
 
-# Create new folder if does not exist
-if not os.path.exists(folder_name):
-    subprocess.run(["mkdir", folder_name])
+# Set-up a folder for metadata
+metadata_folder = "metadata" + "_" + second
+if not os.path.exists(metadata_folder):
+    subprocess.run(["C:\\Windows\\System32\\cmd.exe", "/c", "mkdir", metadata_folder])
+
     
+# Collect metadata
+     
 counter = 0
-    
-while True:
-    # Use subprocess.run to try to run the ffmpeg command in the CL
-    # Chose 75kb because want the images a certain size for training/validating classifier
-    subprocess.run(["ffmpeg", "-i", url_output.stdout.decode('utf-8'), "-vf", "scale=min(1920\,iw):min(1080\,ih):force_original_aspect_ratio=decrease", "-qscale:v", "10", "-fs", "75000", "-f", "image2", f"{folder_name}/img_%03d.jpeg"])
-    
-    # Counter
-    counter += 1
-    if counter >= 2:
+max_iterations = 3
+
+while counter < max_iterations :
+    # Use subprocess.run() to run the youtube-dl command in the CL and assign its output to a variable
+    output = subprocess.run(["youtube-dl", "--write-info-json", "--output", "%(id)s.info.json", url], capture_output=True)
+
+    # Check to see if worked or not
+    if output.returncode != 0:
+        print("youtube-dl command failed with exit code {}".format(output.returncode))
         break
-    
+
     # Sleep between extractions
     time.sleep(interval)
+    counter += 1
+    print("Successfully downloaded metadata for {url} x {counter}".format(url = url, counter = counter))
 
 
+# Iterate over metadata
 
+for filename in os.listdir(metadata_folder):
     
+    # Open the metadata
+    with open(os.path.join(metadata_folder, filename)) as f:
+        metadata = json.load(f)
+    print("Downloading image from {}".format(metadata["thumbnail"]["url"]))
+        
+    # Download the thumbnail image from the URL listed in the metadata
+    response = requests.get(metadata["thumbnail"]["url"])
     
-    
-    
-    #os.system('ffmpeg -i {url_output.stdout.decode('utf-8')} -vcodec copy {folder_name}/img_{timestamp}.jpeg'.format(folder_name=folder_name, timestamp=timestamp))
-    
-
-    
-    #print(f"{url_output.stdout.decode('utf-8')}")
-    
-    #print('"{url_output.stdout.decode('utf-8')}"'.format(url_output=url_output))
-    
-    #print('"{folder_name}/img_{timestamp}.jpeg"'.format(folder_name=folder_name, timestamp=timestamp))
-    
-    
-    
-    
-#this is just what I was using to troubleshoot in the CL
-#ffmpeg -i "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1670598186/ei/yvmSY-XjDtaBsfIPq_-RiAI/ip/104.175.212.63/id/ydYDqZQpim8.37/itag/91/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D139/sgovp/gir%3Dyes%3Bitag%3D160/hls_chunk_host/rr2---sn-a5mekn6z.googlevideo.com/playlist_duration/30/manifest_duration/30/vprv/1/playlist_type/DVR/initcwndbps/2380000/mh/XM/mm/44/mn/sn-a5mekn6z/ms/lva/mv/m/mvi/2/pl/18/dover/11/pacing/0/keepalive/yes/fexp/24001373,24007246/mt/1670576192/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,vprv,playlist_type/sig/AOq0QJ8wRgIhANQUpHE4V_zhA_as4o5KguaZziQLhrkivnGYEqrjuNStAiEAmQZz2vxsw-Mf4yRZylO-xqFP6aFjtQyzQK_mwWJjyrQ%3D/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRAIgL-dohp-FTx7R9RauV4yV2j1RPaTKq3Q8H3JOkfIDJh4CIERa72KWWRd48CPtMIOK6aovSNJJqpTWijC6vCUI52xn/playlist/index.m3u8" -vcodec copy images_ydYDqZQpim8/img_20221209_010307_821231.jpeg
+    # Save the thumbnail to screencaptures folder
+    with open(os.path.join(screencaptures_folder, metadata["id"] + "." + metadata["thumbnail"]["format"]), "wb") as f:
+        f.write(response.content)
+    print("Image downloaded")
--- code/main.py
+++ code/main.py
@@ -1,1 +1,53 @@
-! pip freeze > requirements.txt
+import json
+import os
+import requests
+import time
+import subprocess
+
+# Set URL of YouTube live video
+url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
+first, second = url.split('=')
+
+# Set the time interval (in seconds) for image extraction
+interval = 10
+
+# Set the name of the folder for images
+screencaptures_folder = "images" + "_" + second
+if not os.path.exists(screencaptures_folder):
+    os.mkdir(screencaptures_folder)
+
+# Set-up a folder for metadata
+metadata_folder = "metadata" + "_" + second
+if not os.path.exists(metadata_folder):
+    os.mkdir(metadata_folder)
+
+# Set the path to the youtube-dl executable
+youtube_dl_path = r"C:\Path\To\youtube-dl.exe"
+
+while True:
+    # Use subprocess.check_output to run the youtube-dl command and capture its output
+    output = subprocess.check_output([youtube_dl_path, "--write-info-json", "--output", "%(id)s.info.json", url])
+
+    # Check the exit code of the youtube-dl command to determine if it was successful
+    if output.returncode != 0:
+        print("youtube-dl command failed with exit code {}".format(output.returncode))
+        break
+
+    # Sleep between extractions
+    time.sleep(interval)
+
+
+# Iterate over metadata files in metadata folder
+
+for filename in os.listdir(metadata_folder):
+    
+    # Open the metadata from the file
+    with open(os.path.join(metadata_folder, filename)) as f:
+        metadata = json.load(f)
+        
+    # Download the thumbnail image from the URL indicated by metadata
+    response = requests.get(metadata["thumbnail"]["url"])
+    
+    # Save thumbnail to screencaptures folder
+    with open(os.path.join(screencaptures_folder, metadata["id"] + "." + metadata["thumbnail"]["format"]), "wb") as f:
+        f.write(response.content)
