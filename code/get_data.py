1025a0ceb43ff1cb635459f8ed3b999c9a44169d
CharlesLehnen <lehnen@usc.edu>
2022-12-09 22:33:16 -0800

    trying to capture images again
    installed  PIL (Python Imaging Library):
    
    conda install -c anaconda pillow
    
    also had to do this in the CL too for some reason:
    
    pip install Pillow
    
    and cv2:
    
    conda install -c conda-forge opencv

--- code/get_data.py
+++ code/get_data.py
@@ -2,8 +2,9 @@ import time
 import subprocess
 import os
 from urllib.parse import urlparse
+from PIL import Image
 
-def download_video(url):
+def download_video(url, capture_interval=5):
     # Set up folder and file structure
     video_folder = os.path.join("data", "videos")
     if not os.path.exists(video_folder):
@@ -22,87 +23,20 @@ def download_video(url):
 
     # Start the youtube_dl_process
     youtube_dl_process = subprocess.Popen(command)
+
+    # Capture images from the video
+    image_folder = os.path.join("data", "images")
+    if not os.path.exists(image_folder):
+        os.mkdir(image_folder)
+        
+    vidcap = cv2.VideoCapture(os.path.join(video_output, f"video_{video_id}.mp4"))
+    success, image = vidcap.read()
+    count = 0
     
-    
-# Set the URL of the YouTube live video and run
-url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
-download_video(url)
-
-
-
-
-'''
-
-import os
-import time
-import subprocess
-import io
-from urllib.parse import urlparse
-from subprocess import TimeoutExpired, CalledProcessError
-import signal
-import threading
-
-
-import importlib.util
-
-# Check if the threading module exists
-threading_spec = importlib.util.find_spec("threading")
-
-# Print the result
-if threading_spec:
-    print("The threading module was imported correctly")
-else:
-    print("The threading module was not imported")
-    
-    
-
-# SET PARAMETERS
-
-timeout = 7
-
-# Set URL of YouTube live video and names
-url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
-parsed_url = urlparse(url)
-video_id = parsed_url.query.split("&")[0].split("=")[1]
-
-
-# Set up folder and file structure
-
-video_folder = os.path.join("data", "videos")
-if not os.path.exists(video_folder):
-    os.mkdir(video_folder)
-
-image_folder = os.path.join("data", "images")
-if not os.path.exists(image_folder):
-    os.mkdir(image_folder)
-   
-  
-# Download video
-
-video_output = os.path.join(os.getcwd(), "data", "videos")
-
-command = [
-    "youtube-dl",
-    "--output", f"{video_output}/video_%(id)s.%(ext)s", # this prevented conflicting naming errors
-    "--no-part", # this prevented videos from being exported as .mp4.part
-    url
-]
-
-# Start the subprocess and run for the specified time
-#result = subprocess.run(command, timeout=timeout)
-
-# Start the youtube_dl_process in a separate thread so that I can kill it while it runs in the background
-youtube_dl_thread = threading.Thread(target=subprocess.run, args=(command,))
-youtube_dl_thread.start()
-
-# Wait for the specified time
-time.sleep(timeout)
-
-# Use the terminate() method to terminate the youtube_dl_process
-youtube_dl_thread.terminate()
-
-# Check the return code to see if the youtube_dl_process was terminated
-if youtube_dl_thread.poll() == -signal.SIGKILL:
-    print("The youtube_dl_process was terminated after running for %d seconds" % timeout)
-    
-'''
+    while success:
+        if count % capture_interval == 0:
+            image_file = os.path.join(image_folder, f"image_{video_id}_{count}.jpg")
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            Image.fromarray(image).save(image_file)
+        success, image = vidcap.read()
+        count += 1
