1e6b4502ed5e07110f2a3c1003fff99ca90e396d
CharlesLehnen <lehnen@usc.edu>
2022-12-10 03:53:54 -0800

    then tried calculating elapsed time to indicate when to capture images, but the math is weird

--- code/capture_images.py
+++ code/capture_images.py
@@ -3,11 +3,12 @@ import os
 import cv2
 from PIL import Image
 
-def capture_images(video_id, capture_interval=5):
+def capture_images(video_id, capture_interval = 5):
     # Capture images from the video
     try:
         # Set the start time
         start_time = time.time()
+        print(f'start time {start_time}')
 
         # Set the output and image folders
         video_output = os.path.join(os.getcwd(), "data", "videos")
@@ -18,31 +19,26 @@ def capture_images(video_id, capture_interval=5):
         # Open the video file
         vidcap = cv2.VideoCapture(os.path.join(video_output, f"video_{video_id}.mp4"))
 
-        # Calculate the capture interval based on the video's frame rate
-        frame_rate = vidcap.get(cv2.CAP_PROP_FPS)
-        frame_interval = int(capture_interval / frame_rate)
-
         # Capture images
         success, image = vidcap.read()
-        frame_count = 0
         image_number = 1
-        last_image_time = time.time()
         while success:
-            success, image = vidcap.read()
-            if not success:
-                break
-
-            # Check if the required time interval has passed since the last image was saved
             current_time = time.time()
-            time_passed = current_time - last_image_time
-            if time_passed >= capture_interval:
+            print(f'current time {current_time}')
+            elapsed_time = (current_time - start_time) / 1000  # Divide by 1000 to get in seconds
+            print(f'start time {start_time}')
+            print(f'elapsed_time {elapsed_time}')
+            if elapsed_time >= capture_interval:
+                print('elapsed_time >= capture_interval')
                 image_number += 1
                 image_file = os.path.join(image_folder, f"image_{video_id}_{image_number}.jpg")
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 Image.fromarray(image).save(image_file)
-                last_image_time = current_time  # Save the time when the image was saved
+                start_time = current_time  # Reset start time to current time after an image is captured and saved
+            success, image = vidcap.read()
 
     except KeyboardInterrupt:
         # Kill the youtube-dl process
         youtube_dl_process.terminate()
-        download_process.termin
+        download_process.terminate()
+                    
