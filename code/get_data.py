acf7ccd84da8d7fa9a4567c871dbfaaea1c1db99
CharlesLehnen <lehnen@usc.edu>
2022-12-09 20:37:40 -0800

    converted to function to pass in url. but still having trouble  terminating youtube-dl

--- code/get_data.py
+++ code/get_data.py
@@ -1,23 +1,92 @@
 import os
 import time
 import subprocess
-import cv2
-import ffmpeg
-import youtube_dl
+import threading
 import io
-from pytube import YouTube
+from urllib.parse import urlparse
+import signal
+
+# Create a global variable to store a reference to the youtube_dl_process
+youtube_dl_process = None
+
+def download_video(url):
+    global youtube_dl_process
+
+    # SET PARAMETERS
+    timeout = 7
+
+    # Set URL of YouTube live video and names
+    parsed_url = urlparse(url)
+    video_id = parsed_url.query.split("&")[0].split("=")[1]
+
+    # Set up folder and file structure
+    video_folder = os.path.join("data", "videos")
+    if not os.path.exists(video_folder):
+        os.mkdir(video_folder)
+    output_file = os.path.join(video_folder, f"video_{video_id}_trimmed.mp4")
+    image_folder = os.path.join("data", "images")
+    if not os.path.exists(image_folder):
+        os.mkdir(image_folder)
+
+    # Download video
+    video_output = os.path.join(os.getcwd(), "data", "videos")
+    command = [
+        "youtube-dl",
+        "--output", f"{video_output}/video_%(id)s.%(ext)s",
+        "--no-part",
+        url
+    ]
+
+    # Start the youtube_dl_process
+    youtube_dl_process = subprocess.Popen(command)
+
+# Set the URL of the YouTube live video
+url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
+
+# Start the download_video() function in a separate thread
+youtube_dl_thread = threading.Thread(target=download_video, args=(url,))
+youtube_dl_thread.start()
+
+# Wait for the specified time
+time.sleep(timeout)
+
+# Use the terminate() method of the youtube_dl_process to terminate the youtube_dl_process
+youtube_dl_process.terminate()
+
+# Check the return code to see if the youtube_dl_process was terminated
+if youtube_dl_process.poll() == -signal.SIGKILL:
+    print("The youtube_dl_process was terminated after running for %d seconds" % timeout)
+
+
+
+'''
+
+import os
+import time
+import subprocess
+import io
 from urllib.parse import urlparse
 from subprocess import TimeoutExpired, CalledProcessError
 import signal
+import threading
 
+
+import importlib.util
+
+# Check if the threading module exists
+threading_spec = importlib.util.find_spec("threading")
+
+# Print the result
+if threading_spec:
+    print("The threading module was imported correctly")
+else:
+    print("The threading module was not imported")
+    
+    
 
 # SET PARAMETERS
 
-INTERVAL = 15
-max_runtime = 7
-video_length = 7
-VIDEO_LENGTH = 7
-
+timeout = 7
 
 # Set URL of YouTube live video and names
 url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
@@ -25,14 +94,11 @@ parsed_url = urlparse(url)
 video_id = parsed_url.query.split("&")[0].split("=")[1]
 
 
-
 # Set up folder and file structure
 
 video_folder = os.path.join("data", "videos")
 if not os.path.exists(video_folder):
     os.mkdir(video_folder)
-
-output_file = os.path.join(video_folder, f"video_{video_id}_trimmed.mp4")
 
 image_folder = os.path.join("data", "images")
 if not os.path.exists(image_folder):
@@ -45,123 +111,26 @@ video_output = os.path.join(os.getcwd(), "data", "videos")
 
 command = [
     "youtube-dl",
-    "--output", f"{video_output}/video_%(id)s.%(ext)s",
-    "--no-part",
+    "--output", f"{video_output}/video_%(id)s.%(ext)s", # this prevented conflicting naming errors
+    "--no-part", # this prevented videos from being exported as .mp4.part
     url
 ]
 
-# Download the video using youtube-dl
-subprocess.run(command, timeout = VIDEO_LENGTH)
+# Start the subprocess and run for the specified time
+#result = subprocess.run(command, timeout=timeout)
 
-'''
+# Start the youtube_dl_process in a separate thread so that I can kill it while it runs in the background
+youtube_dl_thread = threading.Thread(target=subprocess.run, args=(command,))
+youtube_dl_thread.start()
 
-## Set process
-command = ['youtube-dl', '-o', output_file, '--format', 'mp4', url, '|', 'ffmpeg', '-i', '-', '-t', max_runtime, output_file]
-subprocess.Popen(command)
+# Wait for the specified time
+time.sleep(timeout)
 
+# Use the terminate() method to terminate the youtube_dl_process
+youtube_dl_thread.terminate()
 
-# Wait for the process to complete or exceed the maximum runtime
-try:
-    process.wait(timeout=max_runtime)
-except subprocess.TimeoutExpired:
-    # Use the os.kill() function to send the SIGKILL signal directly to the youtube-dl process
-    os.kill(process.pid, signal.SIGKILL)
-    # Print a message indicating that the commands timed out
-    print("Download and processing timed out after {} seconds".format(max_runtime))
-except subprocess.CalledProcessError as e:
-    # Print a message indicating that the commands returned a non-zero exit code
-    print("Download and processing failed with exit code {}".format(e.returncode))
-else:
-    # Print a message indicating that the commands completed successfully
-    print("Download and processing completed successfully")
-finally:
-    # Open the video file that was created by the youtube-dl and ffmpeg commands
-    print("Proces ended")
-    # Perform any other necessary cleanup actions here
-
-
-'''
-'''
-
-
-#this worked some of the time, otherwise export mp4.part
-video_output = os.path.join(os.getcwd(), "data", "videos")
-
-command = [
-    "youtube-dl",
-    "--output", f"{video_output}/video_%(id)s.%(ext)s",
-    url
-]
-
-# Download the video using youtube-dl
-subprocess.run(command, timeout = VIDEO_LENGTH)
-
-
-print("done with stream step")
-
-
-#might need to do this, but will download to my local machine which is clunkier
-#output = youtube_dl.YoutubeDL({"format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"}).extract_info(url, download=True)
-
-## Debug
-if 'returncode' in stream and stream['returncode'] != 0:
-    print("youtube-dl command failed with exit code {}".format(stream['returncode']))
+# Check the return code to see if the youtube_dl_process was terminated
+if youtube_dl_thread.poll() == -signal.SIGKILL:
+    print("The youtube_dl_process was terminated after running for %d seconds" % timeout)
     
-
-
-# Set up video capture using ffmpeg
-ffmpeg_command = "ffmpeg -i {} -acodec copy -vcodec copy -f mpegts pipe:".format(stream["url"])
-ffmpeg_process = subprocess.Popen(ffmpeg_command.split(), stdout=subprocess.PIPE)
-print("done with ffmpeg step")
-
-# Trying to use this buffer process to fix error
-buffer = io.BytesIO()
-buffer.write(ffmpeg_process.stdout.read())
-buffer.seek(0)
-cap = cv2.VideoCapture(buffer)
-print("done with buffer step")
-
-# Get correct video stream
-video_stream = cap.get(cv2.CAP_PROP_POS_AVI_RATIO)
-print("done with video_stream step")
-
-
-# Capture frames from the stream
-
-start_time = time.time()
-print(start_time)
-
-while cap.isOpened():
-    # Capture a frame
-    ret, frame = cap.read(video_stream)
-    if ret:
-        # Save the frame
-        cv2.imwrite(os.path.join(video_folder, "clip_{:05d}.png".format(counter)), frame)
-        
-        ## Debug
-        if len(os.listdir(video_folder)) > 0:
-            print("Video frames saved to: {}".format(video_folder))
-        else:
-            print("Error saving video frames to: {}".format(video_folder))
-
-        # Extract an image
-        cv2.imwrite(os.path.join(image_folder, "image_{:05d}.png".format(counter)), image)
-        
-        ## Debug
-        if len(os.listdir(image_folder)) > 0:
-            print("Images saved to: {}".format(image_folder))
-        else:
-            print("Error saving images to: {}".format(image_folder))
-
-        # Sleep
-        time.sleep(INTERVAL)
-        counter += 1
-       
-    # Stop capture
-    elapsed_time = time.time() - start_time
-    if elapsed_time <= MAX_RUNTIME:
-        cap.release()
-        
-        
 '''
-     
