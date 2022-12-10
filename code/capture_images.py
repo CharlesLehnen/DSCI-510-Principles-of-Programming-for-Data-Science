42935e7d75e753464b866819ceb1e251a05eb059
CharlesLehnen <lehnen@usc.edu>
2022-12-09 23:00:30 -0800

    intializing 2 separate scripts for downlaod video versus image capture

---
+++ code/capture_images.py
@@ -0,0 +1,27 @@
+import time
+import os
+import cv2
+from PIL import Image
+
+def capture_images(video_id, capture_interval=1):
+    # Capture images from the video
+    try:
+        start_time = time.time()
+        video_output = os.path.join(os.getcwd(), "data", "videos")
+        image_folder = os.path.join("data", "images")
+        if not os.path.exists(image_folder):
+            os.mkdir(image_folder)
+        vidcap = cv2.VideoCapture(os.path.join(video_output, f"video_{video_id}.mp4"))
+        success, image = vidcap.read()
+        count = 0
+        while success:
+            if count % capture_interval == 0:
+                image_file = os.path.join(image_folder, f"image_{video_id}_{count}.jpg")
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                Image.fromarray(image).save(image_file)
+            success, image = vidcap.read()
+            count += 1
+    except KeyboardInterrupt:
+        end_time = time.time()
+        runtime = end_time - start_time
+        print(f"Total runtime: {runtime} seconds")
--- code/download_video.py
+++ code/download_video.py
@@ -1,10 +1,10 @@
-import time
-import subprocess
 import os
+import subprocess
 from urllib.parse import urlparse
-from PIL import Image
+import multiprocessing
+import capture_images
 
-def download_video(url, capture_interval=1):
+def download_video(url):
     # Set up folder and file structure
     video_folder = os.path.join("data", "videos")
     if not os.path.exists(video_folder):
@@ -25,27 +25,14 @@ def download_video(url, capture_interval=1):
     youtube_dl_process = subprocess.Popen(command)
 
     # Capture images from the video
-    try:
-        start_time = time.time()
-        image_folder = os.path.join("data", "images")
-        if not os.path.exists(image_folder):
-            os.mkdir(image_folder)
-        vidcap = cv2.VideoCapture(os.path.join(video_output, f"video_{video_id}.mp4"))
-        success, image = vidcap.read()
-        count = 0
-        while success:
-            if count % capture_interval == 0:
-                image_file = os.path.join(image_folder, f"image_{video_id}_{count}.jpg")
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                Image.fromarray(image).save(image_file)
-            success, image = vidcap.read()
-            count += 1
-    except KeyboardInterrupt:
-        end_time = time.time()
-        runtime = end_time - start_time
-        print(f"Total runtime: {runtime} seconds")
+    image_capture_process = multiprocessing.Process(
+        target=capture_images.capture_images,
+        args=(video_id,)
+    )
+    image_capture_process.start()
 
 if __name__ == "__main__":
     # Set the URL of the YouTube live video and run
     url = "https://www.youtube.com/watch?v=ydYDqZQpim8"
-    download_video(url)
+    download_process = multiprocessing.Process(target=download_video, args=(url,))
+    download_process.start()
