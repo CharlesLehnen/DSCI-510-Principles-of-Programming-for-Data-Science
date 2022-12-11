9e040c0571f4af68f40403cf9295b889c74c0564
CharlesLehnen <lehnen@usc.edu>
2022-12-11 06:40:03 -0800

    trying to install ILSVRC2012_devkit_t12.tar.gz

--- code/image_classification.py
+++ code/image_classification.py
@@ -5,12 +5,26 @@ import cv2
 from PIL import Image
 from torchvision import transforms
 from torchvision.models.vgg import VGG16_Weights
+import subprocess
+
+# Install ILSVRC2012_devkit_t12 if not already installed
+
+## Set the root directory and file name
+root_dir = "data/imagenet"
+file_name = "ILSVRC2012_devkit_t12.tar.gz"
+file_url = "http://www.image-net.org/challenges/LSVRC/2012/nnoupb/" + file_name
+
+## Check if the file is already present
+if not os.path.exists(os.path.join(root_dir, file_name)):
+    # Download the file
+    subprocess.run(["wget", file_url, "-P", root_dir])
 
 
 FILE_PATH = "data/images/archived"
 
 # Load VggNet model
 model = torchvision.models.vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
+imagenet_dataset = torchvision.datasets.ImageNet(root="data/imagenet", split="val")
 
 # Define preprocessing transformation
 transform = transforms.Compose([
@@ -31,13 +45,21 @@ for filename in os.listdir(FILE_PATH):
 
     # Get the class with the highest probability
     _, predicted = torch.max(output, 1)
+    
+    # Print the predicted classes
+    class_names = imagenet_dataset.label_mapping
+    predicted_class_name = imagenet_dataset.label_mapping[predicted]
+    print("Predicted class index:", predicted)
+    print("Predicted class names:", predicted_class_name)
 
-    # Use the VGG16 model to detect animals
     # Use the VGG16 model to detect animals
     if predicted == "animals":
         # Use OpenCV to detect animals in the image
         animal_cascade = cv2.CascadeClassifier("data/cascades/animals.xml")
         animals = animal_cascade.detectMultiScale(image, scaleFactor=1.05, minNeighbors=5)
+        
+        # Print the number of detected animals
+        print("Number of detected animals:", len(animals))
 
         # Loop through detected animals
         for (x, y, w, h) in animals:
