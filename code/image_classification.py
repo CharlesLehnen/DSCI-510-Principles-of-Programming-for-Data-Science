e8ea3a38730dc679411bd84f122efef7aa66c430
CharlesLehnen <lehnen@usc.edu>
2022-12-11 13:43:16 -0800

    worked!!! issue was model and bounding box function were expecting different image types

--- code/image_classification.py
+++ code/image_classification.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from torchvision.io.image import read_image
 from torchvision.utils import draw_bounding_boxes
@@ -23,12 +24,19 @@ if not os.path.exists(cropped_path):
 for entry in os.scandir(data_path):
     # Step 3a: If the entry is not a directory, process it
     if not entry.is_dir():
-        # Step 3b: Read the image and convert it to a tensor
+        # Read the image and convert it to a tensor
         img_tensor = read_image(entry.path)
+        # Convert the tensor to a floating-point type
+        img_tensor = img_tensor.to(dtype=torch.float32)
         # Step 3c: Use the Faster R-CNN model to get a prediction for the image
         prediction = model([img_tensor], targets=None)[0]
+        
         # Step 3d: Draw bounding boxes around the animals in the image
         labels = [weights.meta["categories"][i] for i in prediction["labels"]]
+        
+        # Convert the tensor to the torch.uint8 type
+        img_tensor = img_tensor.to(dtype=torch.uint8)
+        
         box = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=labels,
                                   colors="red", width=4, font_size=30)
         # Step 3e: Convert the image to a PIL Image
