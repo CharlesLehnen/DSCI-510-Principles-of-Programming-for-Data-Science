779cbc8ef869bc2c8b5c47ab0ca591331a1c3a6d
CharlesLehnen <lehnen@usc.edu>
2022-12-11 14:26:26 -0800

    worked! Went back to original class example and modified it, draws bounding boxes around the test images!

--- code/image_classification.py
+++ code/image_classification.py
@@ -1,53 +1,47 @@
 import os
-import torch
-from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from torchvision.io.image import read_image
+from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
 from torchvision.utils import draw_bounding_boxes
 from torchvision.transforms.functional import to_pil_image
-from PIL import Image
 
-# Change to the path of your images folder
-data_path = "data/images"
+# Directory containing images to process
+image_dir = "data/images"
 
-# Step 1: Initialize the Faster R-CNN model with the best available weights
-weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
-model = fasterrcnn_resnet50_fpn(weights=weights, box_score_thresh=0.9)
-# Set the model to evaluation mode
+# Directory to save the cropped images
+cropped_dir = os.path.join(image_dir, "cropped")
+
+# Create the cropped directory if it doesn't exist
+if not os.path.exists(cropped_dir):
+    os.makedirs(cropped_dir)
+
+# Step 1: Initialize model with the best available weights
+weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
+model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.9)
 model.eval()
 
-# Print the contents of the weights.meta["categories"] dictionary
-print(weights.meta["categories"])
+# Step 2: Initialize the inference transforms
+preprocess = weights.transforms()
 
-'''
-['__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
-'''
+# Process all images in the image directory
+for filename in os.listdir(image_dir):
+    # Skip subdirectories
+    if os.path.isdir(os.path.join(image_dir, filename)):
+        continue
 
-# Step 2: Create a new folder to store the cropped images
-cropped_path = "data/images/cropped"
-if not os.path.exists(cropped_path):
-    os.mkdir(cropped_path)
+    # Read the image
+    img = read_image(os.path.join(image_dir, filename))
 
-# Step 3: Iterate over all entries in the data folder
-for entry in os.scandir(data_path):
-    # Step 3a: If the entry is not a directory, process it
-    if not entry.is_dir():
-        # Read the image and convert it to a tensor
-        img_tensor = read_image(entry.path)
-        # Convert the tensor to a floating-point type
-        img_tensor = img_tensor.to(dtype=torch.float32)
-        # Step 3c: Use the Faster R-CNN model to get a prediction for the image
-        prediction = model([img_tensor], targets=None)[0]
-        
-        # Step 3d: Draw bounding boxes around the animals in the image
-        categories = weights.meta["categories"]
-        labels = [categories[i] for i in prediction["labels"]]
-        
-        # Convert the tensor to the torch.uint8 type
-        img_tensor = img_tensor.to(dtype=torch.uint8)
-        
-        box = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=labels,
-                                  colors="red", width=4, font_size=30)
-        # Step 3e: Convert the image to a PIL Image
-        with to_pil_image(box.detach()) as im:
-            # Step 3f: Save the image to the cropped folder
-            im.save(os.path.join(cropped_path, entry.name))
+    # Apply inference preprocessing transforms
+    batch = [preprocess(img)]
+
+    # Use the model and visualize the prediction
+    prediction = model(batch)[0]
+    labels = [weights.meta["categories"][i] for i in prediction["labels"]]
+    box = draw_bounding_boxes(img, boxes=prediction["boxes"],
+                              labels=labels,
+                              colors="red",
+                              width=4, font_size=30)
+    im = to_pil_image(box.detach())
+
+    # Save the cropped image
+    im.save(os.path.join(cropped_dir, filename))
