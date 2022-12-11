43a8985801148c16a4c036e2d2e538a519678436
CharlesLehnen <lehnen@usc.edu>
2022-12-11 05:47:25 -0800

    I finally got something working! Right now just prints confidence
    why I am trying VggNet:
    
    https://www.researchgate.net/publication/333045049_Classification_of_Animals_with_Different_Deep_Learning_Models

--- code/image_classification.py
+++ code/image_classification.py
@@ -1,69 +1,41 @@
 import os
-import random
+import torch
 import torchvision
+import cv2
 from PIL import Image
-import torchvision.transforms as transforms
+from torchvision import transforms
 
-# Set path
-IMAGE_FOLDER = "data/images/archived"
+FILE_PATH = "data/images/archived"
 
-# Create empthy lists for training, validation, and test image sets
-training_set = []
-validation_set = []
-test_set = []
+# Load VggNet model
+model = torchvision.models.vgg16(pretrained=True)
 
-# Randomly assign approximately 70% images to training, 20% to validation, and %10 to test
-for file_name in os.listdir(IMAGE_FOLDER):
-    file_path = os.path.join(IMAGE_FOLDER, file_name)
-    
-    # Random binanry
-    random_number = random.random()
-    
-    # Add the file to the appropriate set (training, validation, or test)
-    if random_number < 0.7:
-        training_set.append(file_path)
-    elif random_number < 0.9:
-        validation_set.append(file_path)
-    else:
-        test_set.append(file_path)
+# Define preprocessing transformation
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
-# # Create empty lists for the updated image sets
-# updated_training_set = []
-# updated_validation_set = []
-# updated_test_set = []
+# Loop through images
+for filename in os.listdir(FILE_PATH):
+  # Load the image and convert it to a PIL.Image
+  image = cv2.imread(os.path.join(FILE_PATH, filename))
+  image = Image.fromarray(image)
 
-# # Update file paths using os.path.join so that I don't have problems
-# for image in training_set:
-#     updated_image = os.path.join(IMAGE_FOLDER, image)
-#     updated_training_set.append(updated_image)
-    
-# for image in validation_set:
-#     updated_image = os.path.join(IMAGE_FOLDER, image)
-#     updated_validation_set.append(updated_image)
-    
-# for image in test_set:
-#     updated_image = os.path.join(IMAGE_FOLDER, image)
-#     updated_test_set.append(updated_image)
-    
-# Load the model
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn()
+  # Apply the transformation
+  image = transform(image)
 
-# Set the model to evaluation mode
-model.eval()
+  # Add an extra dimension to the image to make it compatible
+  image = image.unsqueeze(0)
 
-# Loop over the images in your dataset
-for image_path in training_set + validation_set + test_set:
-    # Load the image and convert it to a tensor
-    image = Image.open(image_path)
-    transform = transforms.ToTensor()
-    image_tensor = transform(image)
-    
-    # Use the model to detect objects in the image and draw bounding boxes around them
-    outputs = model(image_tensor)
-    for output in outputs:
-        bbox = output['boxes']  # Coordinates of the bounding box
-        # Use the bounding box coordinates to crop the image
-        cropped_image = image_tensor[:, bbox[0]:bbox[2], bbox[1]:bbox[3]]
-        # Save the cropped image to a folder
-        torchvision.utils.save_image(cropped_image, os.path.join(save_folder, f'{i}.jpg'))
-        i += 1
+  # Use the model to make predictions
+  with torch.no_grad():
+    output = model(image)
+
+  # Get the class with the highest probability
+  _, predicted = torch.max(output, 1)
+
+  # Print predicted class
+  print(predicted)
