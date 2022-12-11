53e2a5153fd8354cc4c62141a5cf55e25ed39711
CharlesLehnen <lehnen@usc.edu>
2022-12-11 06:02:33 -0800

    trying to add in cropping functionality
    Code is running, but not producing anything

--- code/image_classification.py
+++ code/image_classification.py
@@ -4,11 +4,13 @@ import torchvision
 import cv2
 from PIL import Image
 from torchvision import transforms
+from torchvision.models.vgg import VGG16_Weights
+
 
 FILE_PATH = "data/images/archived"
 
 # Load VggNet model
-model = torchvision.models.vgg16(pretrained=True)
+model = torchvision.models.vgg16(weights=VGG16_Weights.IMAGENET1K_V1)
 
 # Define preprocessing transformation
 transform = transforms.Compose([
@@ -20,22 +22,32 @@ transform = transforms.Compose([
 
 # Loop through images
 for filename in os.listdir(FILE_PATH):
-  # Load the image and convert it to a PIL.Image
-  image = cv2.imread(os.path.join(FILE_PATH, filename))
-  image = Image.fromarray(image)
+    # Load the image and convert it to a PIL.Image
+    image = cv2.imread(os.path.join(FILE_PATH, filename))
 
-  # Apply the transformation
-  image = transform(image)
+    # Use the model to make predictions
+    with torch.no_grad():
+        output = model(transform(Image.fromarray(image)).unsqueeze(0))
 
-  # Add an extra dimension to the image to make it compatible
-  image = image.unsqueeze(0)
+    # Get the class with the highest probability
+    _, predicted = torch.max(output, 1)
 
-  # Use the model to make predictions
-  with torch.no_grad():
-    output = model(image)
+    # Use the VGG16 model to detect animals
+    # Use the VGG16 model to detect animals
+    if predicted == "animals":
+        # Use OpenCV to detect animals in the image
+        animal_cascade = cv2.CascadeClassifier("data/cascades/animals.xml")
+        animals = animal_cascade.detectMultiScale(image, scaleFactor=1.05, minNeighbors=5)
 
-  # Get the class with the highest probability
-  _, predicted = torch.max(output, 1)
+        # Loop through detected animals
+        for (x, y, w, h) in animals:
+            # Create a bounding box around the animal
+            cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-  # Print predicted class
-  print(predicted)
+            # Crop the image to the bounding box and save it
+            cropped_image = image[y:y+h, x:x+w]
+            cv2.imwrite(f"cropped/{filename}", cropped_image)
+            if not os.path.exists(cropped):
+                os.mkdir(cropped)
+            cv2.imwrite(f"cropped/{filename}", cropped_image)
+
