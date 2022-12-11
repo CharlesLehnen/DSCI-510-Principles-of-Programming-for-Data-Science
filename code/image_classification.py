e529d9439a3cceae36a506f40ae00a57ade3acde
CharlesLehnen <lehnen@usc.edu>
2022-12-11 13:28:24 -0800

    Switched from PIL image to reading directly through torchvision function to try to open image as tensor oject

--- code/image_classification.py
+++ code/image_classification.py
@@ -1,14 +1,12 @@
 import os
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from torchvision.io.image import read_image
-from torchvision.utils import draw_bounding_boxes, save_image
+from torchvision.utils import draw_bounding_boxes
 from torchvision.transforms.functional import to_pil_image
 from PIL import Image
 
 # Change to the path of your images folder
 data_path = "data/images"
-# Make the file readable by everyone
-os.chmod(data_path, 0o644)
 
 # Step 1: Initialize the Faster R-CNN model with the best available weights
 weights = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
@@ -20,27 +18,20 @@ model.eval()
 cropped_path = "data/images/cropped"
 if not os.path.exists(cropped_path):
     os.mkdir(cropped_path)
-    os.chmod(cropped_path, 0o644)
 
 # Step 3: Iterate over all entries in the data folder
-with os.scandir(data_path) as entries:
-    for entry in entries:
-        # Step 3a: If the entry is not a directory, process it
-        if not entry.is_dir():
-            # Step 3b: Read the image
-            img = Image.open(entry.path)
-            # Step 3c: Convert the image to a torch.Tensor object
-            img_tensor = to_tensor(img)
-            # Step 3d: Use the Faster R-CNN model to get a prediction for the image
-            prediction = model([img_tensor], targets=None)[0]
-            # Step 3e: Draw bounding boxes around the animals in the image
-            labels = [weights.meta["categories"][i] for i in prediction["labels"]]
-            box = draw_bounding_boxes(img, boxes=prediction["boxes"], labels=labels,
-                                      colors="red", width=4, font_size=30)
-            # Step 3f: Convert the image to a PIL Image
-            im = to_pil_image(box.detach())
+for entry in os.scandir(data_path):
+    # Step 3a: If the entry is not a directory, process it
+    if not entry.is_dir():
+        # Step 3b: Read the image and convert it to a tensor
+        img_tensor = read_image(entry.path)
+        # Step 3c: Use the Faster R-CNN model to get a prediction for the image
+        prediction = model([img_tensor], targets=None)[0]
+        # Step 3d: Draw bounding boxes around the animals in the image
+        labels = [weights.meta["categories"][i] for i in prediction["labels"]]
+        box = draw_bounding_boxes(img_tensor, boxes=prediction["boxes"], labels=labels,
+                                  colors="red", width=4, font_size=30)
+        # Step 3e: Convert the image to a PIL Image
+        with to_pil_image(box.detach()) as im:
             # Step 3f: Save the image to the cropped folder
             im.save(os.path.join(cropped_path, entry.name))
-            
-
-
