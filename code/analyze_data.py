e604c29fb14bd1a72fafd008065b77f765acd961
CharlesLehnen <lehnen@usc.edu>
2022-12-13 01:32:07 -0800

    Created pandas dataframe from dict featuring counts of images that match the key

--- code/analyze_data.py
+++ code/analyze_data.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 # Create empty dict
 file_dict = {}
@@ -12,7 +13,7 @@ for file in files:
     parts = file.split('_')
     if parts[0] == 'image':
         video_id = parts[1]
-        timestamp = parts[2]
+        timestamp = parts[2] + "_" + parts[3]
         # Create a key for the dictionary using the video_id and timestamp
         key = f"{video_id}_{timestamp}"
         # If the key doesn't exist in the dictionary, create a new list for it
@@ -24,3 +25,13 @@ for file in files:
 # Print results
 print(files)
 print(file_dict)
+
+# Create pandas dataframe from the dictionary
+df = pd.DataFrame(columns=['video_id', 'timestamp', 'count'])
+for key, value in file_dict.items():
+    timestamp = key
+    count = len(value)
+    df = pd.concat([df, {'video_id': video_id, 'timestamp': timestamp, 'count': count}], ignore_index=True)
+
+# Print the dataframe
+df
