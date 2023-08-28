833b1703f02e4914228dc3ca04e4ac15ff7d783d
CharlesLehnen <lehnen@usc.edu>
2023-08-28 11:11:39 -0700

    installing youtube-dl
    conda install -c conda-forge youtube-dl

--- code/download_video.py
+++ code/download_video.py
@@ -10,15 +10,24 @@ from PIL import Image
 
 def download_video(url):
     try :
-        # Set up folder and file structure
-        data_folder = os.path.join("data")
+        # Print current working directory for debugging
+        print(f"Current working directory: {os.getcwd()}")
+
+        # Set up folder and file structure using absolute paths
+        base_dir = os.path.abspath(os.getcwd())
+        data_folder = os.path.join(base_dir, "data")
+        video_folder = os.path.join(data_folder, "videos")
+
+        # Print paths for debugging
+        print(f"Data folder: {data_folder}")
+        print(f"Video folder: {video_folder}")
+
         if not os.path.exists(data_folder):
             os.mkdir(data_folder)
         
-        video_folder = os.path.join(data_folder, "videos")
         if not os.path.exists(video_folder):
             os.mkdir(video_folder)
-            
+
         # Download video
         video_output = os.path.join(os.getcwd(), "data", "videos")
         command = [
